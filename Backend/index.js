5353a7f9c6f61369074358f2962db8e44a25a3d1
Beauty Kumari <116947589+beauty8700@users.noreply.github.com>
2026-06-09 20:41:47 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,12 +9,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: true,
+    credentials: true,
   })
 );
 app.use(express.json());
